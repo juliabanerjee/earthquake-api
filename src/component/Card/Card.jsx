5625009384589f946af5b95c33f2fa084adc091d
Julia Banerjee <juliabanerjee27@gmail.com>
2021-12-23 17:04:02 +0000

    some styling done

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -14,14 +14,14 @@ const {location, magnitude, tsunami, furtherInfo} = props;
   return(
 
 <div className= "card">
-
+<div className="card__h2-image">
 <h2 className="card__h2">{location}</h2>
 <img src={epicentre} alt="earthquake epicentre diagram" className="card__image"/>
+</div>
 
-
-<p>Magnitude: {magnitude}</p>
-<p>Tsunami: {tsunami}</p>
-<p>Further Info: {furtherInfo}</p>
+<p><span className="make-bold">Magnitude:</span> {magnitude}</p>
+<p><span className="make-bold">Tsunami: </span> {tsunami}</p>
+<p><span className="make-bold">Further Info:</span> {furtherInfo}</p>
 
 </div>
 
