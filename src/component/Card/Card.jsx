5b64eecebaff6efb7fd88d1a73110551ba8a7c6e
Julia Banerjee <juliabanerjee27@gmail.com>
2022-03-28 15:01:33 +0100

    styled desktop and mobile cards to look more uniform

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -14,15 +14,17 @@ const {location, magnitude, tsunami, furtherInfo} = props;
   return(
 
 <div className= "card">
-<div className="card__section1">
-<h2 className="card__section1__h2">{location}</h2>
-<img src={epicentre} alt="earthquake epicentre diagram" className="card__section1__image"/>
+<div className="card__container1">
+<h2 className="card__container1__h2">{location}</h2>
 </div>
-
-<p className="card__p"><span className="make-bold">Magnitude:</span> {magnitude}</p>
-<p className="card__p"><span className="make-bold">Tsunami: </span> {tsunami}</p>
-<p className="card__p">Further Info: <a className="card__further-info" href={furtherInfo}>{furtherInfo}</a></p>
-
+<div className="card__container2">
+<img src={epicentre} alt="earthquake epicentre diagram" className="card__container2__image"/>
+</div>
+<div className="card__container3">
+<p className="card__container3__p"><span className="make-bold">Magnitude:</span> {magnitude}</p>
+<p className="card__container3__p"><span className="make-bold">Tsunami: </span> {tsunami}</p>
+<p className="card__container3__p">Further Info: <a className="card__container3__further-info" href={furtherInfo}>{furtherInfo}</a></p>
+</div>
 
 </div>
 
